c47b8834ee2ac71616738080688bb62ed5b763b0
Giorgio Vidali <gvidali@stuy.edu>
2017-05-26 14:50:51 -0400

    writing that good

--- Tank.java
+++ Tank.java
@@ -1,6 +1,14 @@
 public class Tank{
+  private char forward, left, right, back, shoot;
+
+  public Tank(char f, char l, char r, char b, char s){
+    forward = f;
+    left = l;
+    right = r;
+    bacl = b;
+    shoot = s;
+  }
 
 
 
-  
 }
